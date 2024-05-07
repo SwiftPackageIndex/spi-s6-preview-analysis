de24cb759b9bf00e949570cd16c341385b604fa0
Sven A. Schmidt <sas@finestructure.co>
2024-05-07 11:30:13 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "spi-s6-preview-analysis",
+    targets: [
+        .executableTarget(name: "spi-s6-preview-analysis"),
+    ]
+)
