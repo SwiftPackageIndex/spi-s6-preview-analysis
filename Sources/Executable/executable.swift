77e9dc3f0c071320a6567b2bb3133a3c8957950c
Sven A. Schmidt <sas@finestructure.co>
2024-05-08 11:01:47 +0200

    Cleanup

--- Sources/Executable/executable.swift
+++ Sources/Executable/executable.swift
@@ -14,31 +14,28 @@ struct S6Analysis: ParsableCommand {
         let packageLists = try analysis.loadPackageLists()
         let records = try analysis.loadRecords()
 
-        // total error count
-        // - for each date in results
-        // - join all package list with results
-        // - get total error count (get the max across platforms)
+        do {  // Analyse total error count
+            var errorCountOutput = [Analysis.Output]()
+            for listId in ["all", "apple", "sswg"] {
+                print("all packages...")
+                guard let packageList = packageLists[id: listId] else {
+                    fatalError("Package list 'All' not found")
+                }
+                var output = Analysis.Output(id: packageList.id, name: packageList.name, values: [])
+                for (date, results) in records {
+                    print("date:", date, results.count)
+                    let selectetResults = results.filter(by: packageList.packages)
+                    print("selected results:", selectetResults.count)
+                    let errorTotal = selectetResults.errorTotal()
+                    print("errorTotal:", errorTotal)
+                    output.values.append(.init(date: date, value: errorTotal))
+                }
+                errorCountOutput.append(output)
+            }
 
-        var errorCountOutput = [Analysis.Output]()
-        for listId in ["all", "apple", "sswg"] {
-            print("all packages...")
-            guard let packageList = packageLists[id: listId] else {
-                fatalError("Package list 'All' not found")
-            }
-            var output = Analysis.Output(id: packageList.id, name: packageList.name, values: [])
-            for (date, results) in records {
-                print("date:", date, results.count)
-                let selectetResults = results.filter(by: packageList.packages)
-                print("selected results:", selectetResults.count)
-                let errorTotal = selectetResults.errorTotal()
-                print("errorTotal:", errorTotal)
-                output.values.append(.init(date: date, value: errorTotal))
-            }
-            errorCountOutput.append(output)
+            let data = try encoder.encode(errorCountOutput)
+            try data.write(to: URL(relativePath: "rfs6-errors.json"))
         }
-
-        let data = try encoder.encode(errorCountOutput)
-        try data.write(to: URL(relativePath: "rfs6-errors.json"))
     }
 }
 
